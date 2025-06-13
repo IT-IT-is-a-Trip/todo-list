9dee98c5694bf91107a2938e3a129a90bc04ab81
IT-IT-is-a-trip <alexey135135025@gmail.com>
2025-06-13 18:25:22 +0300

    trying some moves

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { toDoListApp } from "./data";
 const App = toDoListApp
 
-App.tasks.addTask('ahah')
+App.tasks.addTask('ahah', 'lol task')
 App.tasks.getTask('ahah')
+App.tasks.addTask('task 2', 'hehheh 2')
+App.tasks.getTask('task 2')
--- src/tasks.js
+++ src/tasks.js
@@ -1,16 +1,21 @@
 export class Tasks {
     tasks = []
 
-    addTask(taskName, description) {
-        if (!this.tasks[taskName]) return
-        this.tasks.push(taskName)
-        this.tasks[taskName].description = description
+    addTask(taskName, description = '') {
+        // if (this.tasks[taskName]) return
+        const task = {
+            taskName: taskName,
+            description: description
+        }
+        this.tasks.push(task)
     }
     removeTask(taskName) {
         if (!this.tasks[taskName]) return
         this.tasks[taskName] = []
     }
     getTask(taskName) {
-        console.log(this.tasks[taskName].description)
+        this.tasks.forEach((task) => {
+            if (task.taskName == taskName) {console.log(task)}
+    })
     }
 }
